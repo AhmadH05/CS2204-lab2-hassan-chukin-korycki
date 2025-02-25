e613d902ff67431c23238343664563c60e36361f
AhmadH05 <82924522+AhmadH05@users.noreply.github.com>
2025-02-24 20:09:43 -0500

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -28,7 +28,7 @@ module tb ();
 `endif
 
   // Replace tt_um_example with your module name:
-  tt_Hassan_Chukin_Korychi user_project (
+  tt_um_Hassan_Chukin_Korycki (
 
       // Include power ports for the Gate Level test:
 `ifdef GL_TEST
